767f82579d9f719cd64ff31671f4b88558206aa9
unknown <lutfiana592>
2024-05-21 17:34:41 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'UTS BIODATA MOBILE PROGRAMING LANJUTAN',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: ProfilePage(),
-    );
+class StateTeksUtama extends State<TeksUtama> {
+  void incremenIndex(){
+    setState(() {
+      index++;
+    });
   }
-}
-
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  var listNama = [
+    'Dwi Septi Lutfiana', 'Nanda Wiasti', 'Adhitya Pramudita Ramadhan', 'Devarel Albany Putra Almansyah',
+    'Didit Syawaludin', 'Arsyad Maulana', 'Reza Yulisantoso', 'Virda Romadani'
+  ];
+  var listWarna = [
+    Colors.red, Colors.purple, Colors.teal, Colors.lime,
+    Colors.indigo, Colors.deepPurple, Colors.cyan,
+    Colors.blue, Colors.yellow
+  ];
+  int index = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Kenalan Yukk'),
-      
-      backgroundColor: Colors.amberAccent[300],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-/*            AspectRatio(
-              aspectRatio: 16,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(16),
-                child: Image.asset('aassets/images/foto.jpg'),
-              ),
-            ),*/
-
-            Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: Theme.of(context).primaryColor,
-                ),
-              ),
-              padding: const EdgeInsets.all(2),
-              child: const CircleAvatar(
-                radius: 80,
-                backgroundImage: AssetImage('assets/images/foto.jpg'),
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Dwi Septi Lutfiana',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'STI202102449',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Jl. Perjuangan No. 48 Desa Sambirata, Cilongok, Banyumas',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Mahasiswa Aktif Prodi Teknik Informatika STMIK Widya Utama',
-              style: TextStyle(fontSize: 16),
-              ),
-              SizedBox(height: 10),
-            Text(
-              'Kunjungi Instagram saya di @dwisep_t untuk mengenal jauh lebih dekat',
-              style: TextStyle(fontSize: 16),
-            ),
-          ],
+    return Column(
+      children: [
+        Text(
+          'Apa kabar',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 45,
+            fontWeight: FontWeight.bold,)
         ),
-      ),
+        Text(
+          listNama[index % listNama.length],
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 65,
+            fontWeight: FontWeight.bold,
+            color: listWarna[index % listWarna.length]
+          ),
+        )
+      ],
     );
   }
+}
+class TeksUtama extends StatefulWidget {
+  final state = StateTeksUtama();
+  @override
+  State<StatefulWidget> createState() => state;
+}
+
+void main() {
+  TeksUtama teksUtama = TeksUtama();
+  runApp(
+    MaterialApp(
+      title: 'Halo Septi',
+      home: Scaffold(
+        body: Center(
+        child: teksUtama,
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Refresh',
+        child: Icon(Icons.refresh),
+        onPressed: teksUtama.state.incremenIndex,
+        ),
+        ),
+     )
+  );
 }
